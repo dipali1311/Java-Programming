44ed752da26308888b76a76b68e45abd3084681b
Dipali Singh <55615204+dipali1311@users.noreply.github.com>
2020-09-18 22:05:16 +0530

    Update pattern.java

--- src/pattern.java
+++ src/pattern.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 
 public class pattern {
-    public static void main(String[] args)  {
-        int i,j;
-        for(i=1;i<=5;i++) {
-            for(j=5;j>=i;j--)
+    public static void main(String[] args) {
+        int i, j;
+        for (i = 5; i >= 1; i--) {
+            for (j = i; j >0; j--) {
                 System.out.print(j);
+            }
             System.out.println();
         }
-        System.out.println();
     }
 }
+
